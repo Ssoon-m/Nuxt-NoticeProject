721b519616effa3e961a83e315c8381ed6391d61
순진 <tkksm2@gmail.com>
2021-10-20 22:43:37 +0900

    modify login page

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,37 +1,32 @@
 <template>
-  <v-container class="fill-height" style="max-width:450px;">
-    <v-row align="center">
-      <v-col cols="12">
+  <v-container class="fill-height" >
+    <v-row justify="center">
+      <v-col cols="auto">
         <v-card
-            class="mx-auto"
-            max-width="400"
-            min-width="400"
+            width="400"
         >
-          <v-img
-          class="white--text align-end"
-          height="200px"
-          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-          >
-          
-            <v-card-title class="text-center">로그인</v-card-title>
-          </v-img>
+          <v-card-text class="text-center">
+            <div class="purple--text pt-5 text-h5">계정 로그인</div>
+          </v-card-text>
           <validation-observer v-slot="{ handleSubmit }">
               <v-form @submit.prevent="handleSubmit(onSubmit)" id="check-login-form">
-                <v-card-text class="text--primary">
+                <v-card-text class=" px-4 pb-6 text--primary">
+
                   <validation-provider
                       v-slot="{ errors }"
                       name="email"
                       rules="required|email"
-                      
                   >
+                    <v-card-subtitle class="pa-0">Email address</v-card-subtitle>
                     <v-text-field
                         v-model="uid"
                         name="id"
-                        prepend-icon="mdi-email"  
+                        prepend-inner-icon="mdi-email"  
                         label="이메일"
+                        color="purple"
+                        solo
                         :counter="10"
                         :error-messages="errors[0]"
-                        :hide-details="errors[0] ? false : true"
                     />
                       
                   </validation-provider>
@@ -40,42 +35,40 @@
                     name="password"
                     rules="required|password"
                   >
+                    <v-card-subtitle class="pa-0">Password</v-card-subtitle>
                     <v-text-field
                         v-model="password"
                         type="password"
+                        solo
+                        color="purple"
                         label="비밀번호"
-                        prepend-icon="mdi-lock-outline"
+                        prepend-inner-icon="mdi-lock-outline"
                         :error-messages="errors[0]"
-                        :hide-details="errors[0] ? false : true"
                     />
                   </validation-provider>
-                </v-card-text>
 
-                <v-card-actions>
                   <v-row justify="center">
                     <v-col 
                       cols="12"
-                      sm="2">
+                      >
                       <v-btn 
                         type="submit"
                         form="check-login-form"
-                        color="blue white--text" 
+                        color="#60467e" 
+                        height="45"
                         width="100%" 
                         class="white--text">
                           로그인
                       </v-btn>
                     </v-col>
-                    <v-col 
-                      cols="12"
-                      sm="2">
-                      <v-btn @click="signUp" color="white" width="100%">
-                        회원가입
-                      </v-btn>
-                    </v-col>
                   </v-row>
-                </v-card-actions>
+                </v-card-text>
               </v-form>
           </validation-observer>
+                   
+          <div style="height:45px" class="d-flex justify-center align-center grey lighten-3">
+            <div><span class="text-h7">회원이 아니세요?</span>&nbsp<NuxtLink style="color:violet" to="/auth/signUp">회원가입</NuxtLink></div>
+          </div>
         </v-card>
       </v-col>
     </v-row>
