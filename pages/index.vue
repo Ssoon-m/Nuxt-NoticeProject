898afbbd8e56064768c910f940776ef3f61f980e
순진 <tkksm2@gmail.com>
2021-10-28 15:46:29 +0900

    middleware modify

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
+  <NoticeList 
+    :pageNo="pageNo"/>
 </template>
 
 <script>
+import NoticeList from '@/components/page/board/NoticeList'
 
 export default {
 
   components: {
+    NoticeList
+  },
+  data(){
+      return{
+          pageNo : 1
+      }
   },
   middleware: 'authenticated'
 }
