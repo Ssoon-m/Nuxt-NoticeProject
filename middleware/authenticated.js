89488a99246f00fd3573ade2e403c664a0749988
순진 <tkksm2@gmail.com>
2021-10-28 15:35:39 +0900

    modify middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,5 +1,7 @@
 export default function ({ store, redirect }) {
     if (!store.state.nav.loginAuth) {
       return redirect('/auth/login')
+    }else{
+      return redirect('boards/1')
     }
   }
